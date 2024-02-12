8f479765ffb3252ce55a82f201c4e9ac2d1a63d7
ManuFerrer094 <manuferrer094@gmail.com>
2024-02-12 09:55:27 +0100

    navbar addition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { QrGeneratorComponent } from "./qr-generator/qr-generator.component";
 import { QrTextComponent } from './qr-text/qr-text.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, QrGeneratorComponent, QrTextComponent]
+    imports: [RouterOutlet, QrGeneratorComponent, QrTextComponent, NavbarComponent]
 })
 export class AppComponent {
   title = 'qr-generator-front';
